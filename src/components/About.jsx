5afdba3c6c338974d52e384af75c62c078c183bb
Julien <116632934+JulienQ1@users.noreply.github.com>
2026-07-01 02:56:53 +0200

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
+import { SectionWrapper } from '../hoc'
+
+const ServiceCard = ({index, title, icon}) => {
+  return (
+    <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+    variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+    className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+    <div
+    options={{
+      max: 45,
+      scale: 1,
+      speed: 450
+    }}
+    className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px]
+    flex justify-evenly items-center flex-col'
+    >
+    <img 
+    src={icon}
+    alt={title}
+    className='w-16 h-16 object-contain'
+    />
+    <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+    </div>
+    </motion.div>
+    </Tilt>
+  )
+}
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}></h2>
+      </motion.div>
+      <motion.p
+      variants={fadeIn('', '', 0.1, 1)}
+      className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+       Je suis diplômé d'un master en « Creative Technologist » qui ce situait au cœur du quartier dynamique de Paris La Défense, au sein du Centre d’innovation De Vinci. J'ai été plongé avec passion dans l’univers fascinant de la recherche sur l’apprentissage humain "human learning". Le groupe de recherche sur Human learning développe des outils qui favorisent la construction continue des connaissances et du savoir-faire tout au long de la vie, en impliquant des apprenants d’horizons divers à travers des expériences incarnées et multimodales. Voici les technologies que j’utilise pour mes projets et mes recherches !
+      </motion.p>
+      <div className='mt-20 flex flex-wrap gap-10'>
+      {services.map((service, index) => (
+        <ServiceCard key={service.title} index={index} {...service}/>
+      ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, 'About')
